62b91966a2ba00b5aaaa0d0da0c676a999765fcd
comfort-stereo <jakeploskey@gmail.com>
2019-03-07 11:06:57 -0800

    Add comments

--- Atko.GDWeaver/Weaving/WeaveListener.cs
+++ Atko.GDWeaver/Weaving/WeaveListener.cs
@@ -2,6 +2,10 @@ using Godot;
 
 namespace Atko.GDWeaver.Weaving
 {
+    /// <summary>
+    /// Simple node that, when present in the scene tree, will automatically call <see cref="Weaver.Weave"/> on added
+    /// nodes as soon as they enter the tree.
+    /// </summary>
     public class WeaveListener : Node
     {
         internal WeaveListener()
--- Atko.GDWeaver/Weaving/Weaver.cs
+++ Atko.GDWeaver/Weaving/Weaver.cs
@@ -5,6 +5,17 @@ namespace Atko.GDWeaver.Weaving
     public static class Weaver
     {
         /// <summary>
+        /// Install related nodes on a given node according to instances of <see cref="InstallAttribute"/>, then connect
+        /// signals according to instances of <see cref="ConnectAttribute"/>.
+        /// </summary>
+        /// <param name="node"></param>
+        public static void Weave(this Node node)
+        {
+            Installer.Run(node);
+            Connector.Run(node);
+        }
+
+        /// <summary>
         /// Automatically apply <see cref="Weaver.Weave"/> to all nodes when they are added to the scene tree. Should
         /// only be called once or as the scene changes.
         /// </summary>
@@ -12,12 +23,6 @@ namespace Atko.GDWeaver.Weaving
         public static void Inject(SceneTree tree)
         {
             tree.Root.AddChild(new WeaveListener());
-        }
-
-        public static void Weave(this Node node)
-        {
-            Installer.Run(node);
-            Connector.Run(node);
         }
     }
 }
